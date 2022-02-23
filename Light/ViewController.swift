0ccddcb0ad2c40f58f9dd1adda0ed5db0cdface3
Jatin Agrawal <jatinagrawal2000@gmail.com>
2022-02-23 11:12:07 +0530

    Light On Off

--- Light/ViewController.swift
+++ Light/ViewController.swift
@@ -9,11 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lightButton: UIButton!
+    
+    var lightOn : Bool = true
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        lightButton.setTitle("Turn Off", for: .normal)
     }
 
-
+    @IBAction func ButtonPressed(_ sender: Any) {
+        lightOn.toggle()
+        view.backgroundColor = lightOn ? .white : .black
+        lightButton.setTitle(lightOn ? "Turn Off" : "Turn On", for: .normal)
+    }
+    
 }
 
